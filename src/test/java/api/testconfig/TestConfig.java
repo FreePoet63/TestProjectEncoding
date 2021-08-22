c1b434d2433e2f57d4dad26c4b295991e7c02678
FreePoet63 <razlivinskiy1984@gmail.com>
2021-08-22 16:12:38 +0300

    NewTestProjectEncoding

--- src/test/java/api/testconfig/TestConfig.java
+++ src/test/java/api/testconfig/TestConfig.java
@@ -4,7 +4,7 @@ import io.restassured.response.Response;
 
 public class TestConfig {
     public final static String URL_XML = "https://status.encoding.com/status.php";
-    public final static String URL_JSON = "https://status.encoding.com/status.php?format=json";
+    public final static String URL_JSON = "https://status.encoding.com/status.php";
     public static Response response;
     public static String formatAsString;
     public static String formatString;
